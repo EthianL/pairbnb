d591155bdc521eca0cc127cf047d824fcc49f52e
Ethian Lum <ethian1994@gmail.com>
2016-12-22 09:11:52 +0000

    added 'Clearence' for user management

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,36 @@
+enable :sessions
+
 class UsersController < ApplicationController
+   
+   post '/login' do
+   
+    if user = User.authenticate(params[:email], params[:password])
+        
+    session[:current_user_id] = user.id
+    redirect '/home'
+
+    else
+    redirect '/'
+    end   
+end
+
+
+post '/register' do
+   
+    user = User.new(params)
+    if user.save
+    session[:current_user_id] = user.id
+        
+    else
+        
+    redirect '/home'
+    end
+end
+
+
+get '/logout' do
+    
+    session[:current_user_id] = nil
+    redirect '/'
+end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   
-  resources :users
+  resources :users 
+  
+  root 'static_pages#index'
   
   
   # The priority is based upon order of creation: first created -> highest priority.
